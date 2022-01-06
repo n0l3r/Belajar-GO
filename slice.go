f76edfc818e9e5084664456c702efecc01ea32ba
n0l3r <naufal.120140044@student.itera.ac.id>
2022-01-06 13:16:15 +0700

    slice

---
+++ slice.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var months = [...]string{
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	}
+
+	var slice1 = months[2:5]
+	fmt.Println(slice1)
+}
